b19cbeff4b81cb0dc46629444fd2e58669e798b2
Nelio Alves <acenelio@gmail.com>
2017-10-18 10:45:09 -0200

    Primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# oop-csharp-intro1
+# Introdução à OOP - Exemplo de Produtos
+Este programa é um exemplo didático utilizado no capítulo introdutório do Curso Completo de Programação Orientada a Objetos com C#. 
+### Link para o curso
+https://www.udemy.com/programacao-orientada-a-objetos-csharp/
+
---
+++ curso/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto P = new Produto();
+            int qte;
+
+            Console.WriteLine("Digite os dados do produto:");
+            Console.Write("Nome: ");
+            P.nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            P.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            P.quantidadeEmEstoque = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + P);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que entraram no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            P.realizarEntrada(qte);
+            Console.WriteLine("Dados atualizados: " + P);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que saíram do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            P.realizarSaida(qte);
+            Console.WriteLine("Dados atualizados: " + P);
+
+            Console.ReadLine();
+        }
+    }
+}
